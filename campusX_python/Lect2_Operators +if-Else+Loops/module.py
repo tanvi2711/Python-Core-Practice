a58eccc1e50cd9a1a24a57d93fde49e8b336182e
tanvi2711 <tanvijivatode2711@gmail.com>
2026-06-01 00:58:40 +0530

    Modules in python

---
+++ campusX_python/Lect2_Operators +if-Else+Loops/module.py
@@ -0,0 +1,29 @@
+# Modules in Python
+# math
+# keywords
+# random
+# datetime
+
+import math
+
+print(math.factorial(6))
+print(math.floor(6.6))
+print(math.sqrt(4))
+
+
+
+import keyword
+
+print(keyword.kwlist)
+
+
+import random
+
+print(random.randint(1,100))
+
+
+import datetime
+print(datetime.datetime.now())
+
+
+help('modules')
