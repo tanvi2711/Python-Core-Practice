61fd0fc0ab14382f72922313487ac78e627b8db2
tanvi2711 <tanvijivatode2711@gmail.com>
2026-06-03 19:49:16 +0530

    create files to solve problem

---
+++ campusX_python/Lect3_Python_Strings/PracticeSet/Problem1.py
@@ -0,0 +1,12 @@
+# Problem 1 - Print the following pattern. Write a program to use for loop to print the following reverse number pattern.
+
+
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+num=int(input("Enter no of rows": ))
+
+
