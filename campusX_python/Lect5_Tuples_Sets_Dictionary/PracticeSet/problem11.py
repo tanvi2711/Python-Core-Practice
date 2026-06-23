a30e0f2c33e284d546790e43ad37977ac320cee0
tanvi2711 <tanvijivatode2711@gmail.com>
2026-06-23 23:01:59 +0530

    Problem 11 is solved

--- campusX_python/Lect5_Tuples_Sets_Dictionary/PracticeSet/problem11.py
+++ campusX_python/Lect5_Tuples_Sets_Dictionary/PracticeSet/problem11.py
@@ -10,8 +10,14 @@
 # Output:
 # CampusX
 
-test_dict = {"CampusX" : [5, 7, 9, 4, 0], "is" : [6, 7, 4, 3, 3], "Best" : [9, 9, 6, 5, 5]}
+test_dict = {"CampusX" : [5, 7, 7, 7, 7], "is" : [6, 7, 7, 7], "Best" : [9, 9, 6, 5, 5]}
 
+l=[]
 
 for i in test_dict:
-    
+    test_dict[i]=set(test_dict[i])
+    l.append(len(test_dict[i]))
+
+for i in test_dict:
+    if len(test_dict[i])==max(l):
+        print(i)
